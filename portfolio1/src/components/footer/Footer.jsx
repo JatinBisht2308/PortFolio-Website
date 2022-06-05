027c0837b566ab148ea8158e31cf12ed4f096b90
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-06-06 02:06:57 +0530

    :fire: completed whole website

--- portfolio1/src/components/footer/Footer.jsx
+++ portfolio1/src/components/footer/Footer.jsx
@@ -8,29 +8,31 @@ const Footer = () => {
   return (
     <footer>
       <a href="#" className="footer__logo">
-        <JB />
+        <div>
+        <img src={JB} alt="" />
+        </div>
       </a>
 
-      <ul className="permalinks"></ul>
-      <li>
-        <a href="#">Home</a>
-      </li>
-      <li>
-        <a href="#about">About</a>
-      </li>
-      <li>
-        <a href="#experience">Experience</a>
-      </li>
-      <li>
-        <a href="#services">Services</a>
-      </li>
-      <li>
-        <a href="#portfolio">Portfolio</a>
-      </li>
-      <li>
-        <a href="#contact">Contact</a>
-      </li>
-
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
       <div className="footer__socials">
         <a href="https://www.linkedin.com/in/jatin-bisht-ba62111bb/">
           <BsLinkedin />
