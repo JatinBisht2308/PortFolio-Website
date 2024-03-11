b4f6534a8dae28b117c2f6d84c2f05b46571d598
Jatin <jatinbisht2308@gmail.com>
2024-03-12 00:35:59 +0530

    completed the hero section for the desktop

--- portfolio2/src/components/hero/Hero.jsx
+++ portfolio2/src/components/hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroImage from "../../assets/front-avatar.svg";
 import "./style.css";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -7,6 +7,32 @@ import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import XIcon from "@mui/icons-material/X";
 import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
 const Hero = () => {
+  const [isHovered, setIsHovered] = useState([false, false, false, false]);
+  const onMouseOver = (icon) => {
+    let tempHoverArray = [false, false, false, false];
+    switch (icon) {
+      case "linkedin":
+        tempHoverArray[1] = true;
+        break;
+      case "github":
+        tempHoverArray[0] = true;
+        break;
+      case "blog":
+        tempHoverArray[2] = true;
+        break;
+      case "linktree":
+        tempHoverArray[3] = true;
+        break;
+      default:
+        break;
+    }
+    setIsHovered(tempHoverArray);
+  };
+
+  const onMouseLeave = () => {
+    setIsHovered([false, false, false, false]); 
+  };
+  
   return (
     <div className="Hero">
       <div className="left-hero">
@@ -32,25 +58,86 @@ const Hero = () => {
             expectations.
           </p>
         </div>
-        <div className="socials">
-          <div>
+        <div
+          className="socials "
+          style={{
+            display: "flex",
+            width: "35%",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            className="social-handle"
+            onMouseEnter={() => onMouseOver('github')}
+            onMouseLeave={onMouseLeave}
+          >
             <GitHubIcon
               sx={{
                 color: "black",
               }}
+              style={{
+                cursor: "pointer",
+                width: "18px",
+                color: isHovered[0] ? "white" : "black",
+              }}
+              onClick={() => {
+                window.open("https://github.com/JatinBisht2308", "_blank");
+              }}
             />
           </div>
-          <div>
-            <LinkedInIcon />
+          <div
+            className="social-handle"
+            onMouseEnter={() => onMouseOver('linkedin')}
+            onMouseLeave={onMouseLeave}
+          >
+            <LinkedInIcon
+              style={{
+                cursor: "pointer",
+                width: "18px",
+                color: isHovered[1] ? "white" : "black",
+              }}
+              onClick={() => {
+                window.open(
+                  "https://www.linkedin.com/in/jatinbisht2308/",
+                  "_blank"
+                );
+              }}
+            />
           </div>
-          <div>
-            <XIcon />
+          <div
+            className="social-handle"
+            onMouseEnter={() => onMouseOver('blog')}
+            onMouseLeave={onMouseLeave}
+          >
+            <HistoryEduIcon
+              style={{
+                cursor: "pointer",
+                width: "18px",
+                color: isHovered[2] ? "white" : "black",
+              }}
+              onClick={() => {
+                window.open(
+                  "https://jatinonwebdevelopment.hashnode.dev/",
+                  "_blank"
+                );
+              }}
+            />
           </div>
-          <div>
-            <HistoryEduIcon />
-          </div>
-          <div>
-            <BusinessCenterIcon />
+          <div
+            className="social-handle"
+            onMouseEnter={() => onMouseOver('linktree')}
+            onMouseLeave={onMouseLeave}
+          >
+            <BusinessCenterIcon
+              style={{
+                cursor: "pointer",
+                width: "18px",
+                color: isHovered[3] ? "white" : "black",
+              }}
+              onClick={() => {
+                window.open("https://linktr.ee/jatin_bisht", "_blank");
+              }}
+            />
           </div>
         </div>
       </div>
